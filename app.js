3cf4b462773f1a6c909aded02d4333193dae5ce3
umakusumuru <kusumuruuma@gmail.com>
2016-09-30 15:53:11 +0530

    added app file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+var path = require('path');
+app.use(express.static(path.join(__dirname)));
 app.get('/', function(req, res){
     res.sendfile('index.html', { root: __dirname + "/" } );
 });
