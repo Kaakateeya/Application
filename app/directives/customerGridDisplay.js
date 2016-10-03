29f6073beee32b328b987ae58f8b71594a183cd0
lakshmidevi <lakshmidevi407@gmail.com>
2016-10-03 12:46:21 +0530

    Mylakshmi

---
+++ app/directives/customerGridDisplay.js
@@ -0,0 +1,16 @@
+
+
+
+app.directive("partnerData",function () {
+    return{
+    restrict:"E",
+    templateUrl:"templates/commonpartnerprofiles.html",
+   // scope={
+        //data="="
+   // },
+   // link:function(scope,element){
+         //   console.log(scope.data);
+   // }
+    }
+});
+
---
+++ app/modules/dashboard/controllers/dashboardController.js
@@ -0,0 +1,4 @@
+app.controller('Controllerpartner',['$scope',function(scope){
+ scope.sssarr=[{name:"hbh"},{place:"kadapa",profileid:"21090352"}];
+
+}]);
