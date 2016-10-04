b31770f06e1b5505ef124c0b2cb10d2d09a1c8c7
umakusumuru <kusumuruuma@gmail.com>
2016-10-04 15:29:02 +0530

    angular popup added

--- index.js
+++ index.js
@@ -6,13 +6,35 @@
 /**
  * Main App Creation
  */
-var app = angular.module('Kaakateeya', ['ngRoute']);
+var app = angular.module('Kaakateeya', ['ngRoute','ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
 /**
  * Configure the Routes
  */
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-  .when("/home", {templateUrl: "app/modules/dashboard/patientDashboardView.html",controller:"patientDashboardCntrl"})
-    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+    // Home
+    .when("/", { templateUrl: "app/modules/static/dataGetin.html", controller: "ModalDemoCtrl" })
+    // Pages
+    .when("/about", { templateUrl: "partials/about.html", controller: "PageCtrl" })
+    .when("/faq", { templateUrl: "partials/faq.html", controller: "PageCtrl" })
+    .when("/pricing", { templateUrl: "partials/pricing.html", controller: "PageCtrl" })
+    .when("/services", { templateUrl: "partials/services.html", controller: "PageCtrl" })
+    .when("/contact", { templateUrl: "partials/contact.html", controller: "PageCtrl" })
+    // Blog
+    .when("/blog", { templateUrl: "partials/blog.html", controller: "BlogCtrl" })
+    .when("/blog/post", { templateUrl: "partials/blog_item.html", controller: "BlogCtrl" })
+    // else 404
+    .otherwise("/404", { templateUrl: "partials/404.html", controller: "PageCtrl" });
 }]);
+
+app.controller('headctrl', ['$scope', function (scope) {
+  scope.divloginblock = function () {
+
+$('.login_block_header').toggle();
+
+  }
+}]);
+
+
+
