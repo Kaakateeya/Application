2d65a4f99eed535d024df5c0283993179e803368
vinu <vinodanasuri@gmail.com>
2016-10-15 16:59:44 +0530

    Added files for deployment.

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var express = require('express');
-var app = express();
+var app = express.createServer();
 var path = require('path');
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', function(req, res){
-    //res.sendfile('indexdefault.html', { root: __dirname + "/" }) ;
     res.sendfile('index.html', { root: __dirname + "/" }) ;
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.PORT);
 
