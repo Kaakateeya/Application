fe0117f90359362be4a1e51471fc3f4e62ea8284
lakshmidevi <lakshmidevi407@gmail.com>
2016-11-21 19:27:20 +0530

    searches

--- app/modules/search/generalSearchView.js
+++ app/modules/search/generalSearchView.js
@@ -1,7 +1,9 @@
-app.controller('Generalsearch', ['$scope', '$element', function(scope, $element) {
+app.controller('Generalsearch', ['$scope', '$element', 'arrayConstants', 'SelectBindService', function(scope, $element, arrayConstants, service) {
     scope.data = [];
     scope.vegetables = ['Corn', 'Onions', 'Kale', 'Arugula', 'Peas', 'Zucchini'];
     scope.searchTerm;
+    scope.selectcaste;
+
     scope.clearSearchTerm = function() {
         scope.searchTerm = '';
     };
@@ -10,4 +12,38 @@ app.controller('Generalsearch', ['$scope', '$element', function(scope, $element)
     $element.find('input').on('keydown', function(ev) {
         ev.stopPropagation();
     });
+    scope.Age = function() {
+        scope.test = [];
+        scope.test = [{ label: "--Select--", title: "--select--", value: "0" }];
+        for (var i = 18; i < 78; i++) {
+            scope.test.push({ label: i + ' years', title: i + ' years', value: i });
+        }
+        return scope.test;
+    };
+    scope.arrayAge = scope.Age();
+    scope.height = arrayConstants.height;
+    scope.MaritalStatus = arrayConstants.MaritalStatus;
+    scope.educationcategory = arrayConstants.educationcategory;
+    service.countrySelect().then(function(response) {
+        scope.Country = [];
+        _.each(response.data, function(item) {
+            scope.Country.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+        });
+    });
+    scope.Religion = arrayConstants.Religion;
+    scope.Mothertongue = arrayConstants.Mothertongue;
+    service.casteselect().then(function(response) {
+        scope.Caste = [];
+        _.each(response.data, function(item) {
+            scope.Caste.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+        });
+    });
+    scope.selectAllTest = function() {
+
+        debugger;
+        scope.selectcaste = [];
+        _.each(scope.Caste, function(caste) {
+            scope.selectcaste.push(caste.value);
+        });
+    };
 }]);
