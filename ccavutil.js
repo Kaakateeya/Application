b5b4b36bb498e07d174d7979042e293a8d50d5d7
lakshmidevi <lakshmidevi407@gmail.com>
2017-02-09 16:25:13 +0530

    payment errr

--- ccavutil.js
+++ ccavutil.js
@@ -16,7 +16,7 @@ exports.decrypt = function(encText, workingKey) {
     var key = m.digest();
     var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';
     var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-    var decoded = decipher.update(encText);
-    decoded += decipher.final();
+    var decoded = decipher.update(encText, 'hex', 'utf8');
+    decoded += decipher.final('utf8');
     return decoded;
 };
