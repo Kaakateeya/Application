9a2b346fd65835a90a15d8e38066a01537a4dc01
lakshmidevi <lakshmidevi407@gmail.com>
2017-02-09 19:45:18 +0530

    payment and new page customerlogg

--- app/modules/payments/controllers/paymentResponseHandler.js
+++ app/modules/payments/controllers/paymentResponseHandler.js
@@ -5,6 +5,28 @@ app.controller('ccavenueresponsectrl', ['$scope', '$stateParams', '$http', 'aler
         scope.trackingID = '';
         scope.orderStatus = '';
         scope.paymentobject = JSON.parse(sessionStorage.getItem("paymentobject"));
+
+        scope.points = scope.paymentobject.Points;
+        scope.MembershipName = scope.paymentobject.MembershipName;
+        scope.Duration = scope.paymentobject.Duration;
+        scope.MembershipAmount = scope.paymentobject.Amount;
+        scope.ExpiryDate = moment().add(scope.Duration, 'M').format("DD-MM-YYYY");
+        scope.paymentinsert = function() {
+            var obj = {
+                intCustID: scope.paymentobject.CustID,
+                intMembershipID: scope.paymentobject.MembershipID,
+                DiscountID: scope.paymentobject.DiscountID !== "" ? scope.paymentobject.DiscountID : null,
+                Points: scope.paymentobject.Points,
+                MembershipName: scope.paymentobject.MembershipName,
+                Duration: scope.paymentobject.Duration,
+                MembershipAmount: scope.paymentobject.Amount
+            };
+            myAppFactory.Paymentinsert(obj).then(function(response) {
+
+            });
+        };
+
+
         http.post('/decrypt', JSON.stringify({ keyname: stateParams.data })).then(function(response) {
 
             if (response.data !== undefined && response.data !== null) {
@@ -26,24 +48,6 @@ app.controller('ccavenueresponsectrl', ['$scope', '$stateParams', '$http', 'aler
             }
         });
 
-        scope.points = scope.paymentobject.Points;
-        scope.MembershipName = scope.paymentobject.MembershipName;
-        scope.Duration = scope.paymentobject.Duration;
-        scope.MembershipAmount = scope.paymentobject.Amount;
-        scope.ExpiryDate = moment().add(scope.Duration, 'M').format("DD-MM-YYYY");
-        scope.paymentinsert = function() {
-            var obj = {
-                intCustID: scope.paymentobject.CustID,
-                intMembershipID: scope.paymentobject.MembershipID,
-                DiscountID: scope.paymentobject.DiscountID !== "" ? scope.paymentobject.DiscountID : null,
-                Points: scope.paymentobject.Points,
-                MembershipName: scope.paymentobject.MembershipName,
-                Duration: scope.paymentobject.Duration,
-                MembershipAmount: scope.paymentobject.Amount
-            };
-            myAppFactory.Paymentinsert(obj).then(function(response) {
 
-            });
-        };
     }
 ]);
