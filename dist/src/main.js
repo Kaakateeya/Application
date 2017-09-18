be41c34ca86c26de46e53a9ce2a842e08260e99b
umakusumuru <kusumuruuma@gmail.com>
2017-09-18 10:49:10 +0530

    reverted

--- dist/src/main.js
+++ dist/src/main.js
@@ -1058,7 +1058,7 @@ app.directive('multiselectdropdown', ['arrayConstants', 'SelectBindServiceApp', 
                             // });
 
 
-                            scope.databind(countryArrayModel.ProfCatgory);
+                            scope.databind(eduprofArrayModel.ProfCatgory);
 
 
 
@@ -1073,7 +1073,7 @@ app.directive('multiselectdropdown', ['arrayConstants', 'SelectBindServiceApp', 
                             //     scope.databind(option);
                             // });
 
-                            scope.databind(countryArrayModel.ProfGroup);
+                            scope.databind(eduprofArrayModel.ProfGroup);
 
 
                             break;
@@ -1117,7 +1117,7 @@ app.directive('multiselectdropdown', ['arrayConstants', 'SelectBindServiceApp', 
                             //     scope.databind(option);
                             // });
 
-                            scope.databind(countryArrayModel.caste);
+                            scope.databind(otherArrayModel.caste);
                             break;
 
                             // case 'Caste':
@@ -1180,7 +1180,7 @@ app.directive('multiselectdropdown', ['arrayConstants', 'SelectBindServiceApp', 
                             //     });
                             //     scope.databind(option);
                             // });
-                            scope.databind(countryArrayModel.newProfessionCatgory);
+                            scope.databind(eduprofArrayModel.newProfessionCatgory);
 
                             break;
 
@@ -1742,110 +1742,110 @@ app.directive('setClassWhenAtTop', function($window) {
             model.countryCode = [];
             model.currency = [];
             // timeout(function() {
-            // model.Countryf();
-            // model.stateSelectf();
-            // model.countryCodeselectf();
-            // model.currencyf();
-            model.allAtOnce();
+            model.Countryf();
+            model.stateSelectf();
+            model.countryCodeselectf();
+            model.currencyf();
+            // model.allAtOnce();
 
             // });
             return model;
         };
 
-        // model.Countryf = function() {
-        //     if (model.Country.length === 0) {
-        //         service.countrySelect().then(function(response) {
-        //             var option = [];
-        //             option.push({ "label": "--select--", "title": "--select--", "value": "" });
-        //             _.each(response.data, function(item) {
-        //                 option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
-        //             });
-        //             model.Country = option;
-        //         });
-        //     }
-        // };
-
-        // model.stateSelectf = function() {
-
-        //     if (model.IndiaStates.length === 0) {
-        //         service.stateSelect('1').then(function(response) {
-        //             var option = [];
-        //             option.push({ "label": "--select--", "title": "--select--", "value": "" });
-        //             _.each(response.data, function(item) {
-        //                 option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
-        //             });
-        //             model.IndiaStates = option;
-        //         });
-        //     }
-
-        // };
-
-        // model.countryCodeselectf = function() {
-        //     if (model.countryCode.length === 0) {
-        //         service.countryCodeselect().then(function(response) {
-        //             var option = [];
-        //             option.push({ "label": "--select--", "title": "--select--", "value": "" });
-        //             _.each(response.data, function(item) {
-        //                 option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
-        //             });
-        //             model.countryCode = option;
-        //         });
-        //     }
-        // };
-
-        // model.currencyf = function() {
-        //     if (model.currency.length === 0) {
-        //         service.currency().then(function(response) {
-        //             var option = [];
-        //             option.push({ "label": "--select--", "title": "--select--", "value": "" });
-        //             _.each(response.data, function(item) {
-        //                 option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
-        //             });
-        //             model.currency = option;
-        //         });
-        //     }
-        // };
-
-
-        model.allAtOnce = function() {
+        model.Countryf = function() {
             if (model.Country.length === 0) {
-                timeout(function() {
-                    service.getCustomerBindings().then(function(response) {
-                        if (response.data) {
-                            model.IndiaStates = [];
-                            model.countryCode = [];
-                            model.Country = [];
-                            model.profgroup = [];
-                            model.profcatgory = [];
-                            model.currency = [];
-                            model.newProfcatgory = [];
-
-                            model.Country = model.returnFormatArray(JSON.parse(response.data[2]));
-                            model.ProfCatgory = model.returnFormatArray(JSON.parse(response.data[4]));
-                            model.ProfGroup = model.returnFormatArray(JSON.parse(response.data[3]));
-                            model.IndiaStates = model.returnFormatArray(JSON.parse(response.data[0]));
-                            model.countryCode = model.returnFormatArray(JSON.parse(response.data[1]));
-                            model.currency = model.returnFormatArray(JSON.parse(response.data[5]));
-                            model.newProfessionCatgory = model.returnFormatArray(JSON.parse(response.data[6]));
-                            model.caste = model.returnFormatArray(JSON.parse(response.data[7]));
-                            // response.data = JSON.parse(response.data);
-
-                        }
+                service.countrySelect().then(function(response) {
+                    var option = [];
+                    option.push({ "label": "--select--", "title": "--select--", "value": "" });
+                    _.each(response.data, function(item) {
+                        option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
                     });
-                }, 500);
-            }
-        };
-
-        model.returnFormatArray = function(arr) {
-            var option = [];
-            if (arr.length > 0) {
-                option.push({ "label": "--select--", "title": "--select--", "value": "" });
-                _.each(arr, function(item) {
-                    option.push({ "label": item.NAME, "title": item.NAME, "value": item.ID });
+                    model.Country = option;
                 });
             }
-            return option;
         };
+
+        model.stateSelectf = function() {
+
+            if (model.IndiaStates.length === 0) {
+                service.stateSelect('1').then(function(response) {
+                    var option = [];
+                    option.push({ "label": "--select--", "title": "--select--", "value": "" });
+                    _.each(response.data, function(item) {
+                        option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+                    });
+                    model.IndiaStates = option;
+                });
+            }
+
+        };
+
+        model.countryCodeselectf = function() {
+            if (model.countryCode.length === 0) {
+                service.countryCodeselect().then(function(response) {
+                    var option = [];
+                    option.push({ "label": "--select--", "title": "--select--", "value": "" });
+                    _.each(response.data, function(item) {
+                        option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+                    });
+                    model.countryCode = option;
+                });
+            }
+        };
+
+        model.currencyf = function() {
+            if (model.currency.length === 0) {
+                service.currency().then(function(response) {
+                    var option = [];
+                    option.push({ "label": "--select--", "title": "--select--", "value": "" });
+                    _.each(response.data, function(item) {
+                        option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+                    });
+                    model.currency = option;
+                });
+            }
+        };
+
+
+        // model.allAtOnce = function() {
+        //     if (model.Country.length === 0) {
+        //         timeout(function() {
+        //             service.getCustomerBindings().then(function(response) {
+        //                 if (response.data) {
+        //                     model.IndiaStates = [];
+        //                     model.countryCode = [];
+        //                     model.Country = [];
+        //                     model.profgroup = [];
+        //                     model.profcatgory = [];
+        //                     model.currency = [];
+        //                     model.newProfcatgory = [];
+
+        //                     model.Country = model.returnFormatArray(JSON.parse(response.data[2]));
+        //                     model.ProfCatgory = model.returnFormatArray(JSON.parse(response.data[4]));
+        //                     model.ProfGroup = model.returnFormatArray(JSON.parse(response.data[3]));
+        //                     model.IndiaStates = model.returnFormatArray(JSON.parse(response.data[0]));
+        //                     model.countryCode = model.returnFormatArray(JSON.parse(response.data[1]));
+        //                     model.currency = model.returnFormatArray(JSON.parse(response.data[5]));
+        //                     model.newProfessionCatgory = model.returnFormatArray(JSON.parse(response.data[6]));
+        //                     model.caste = model.returnFormatArray(JSON.parse(response.data[7]));
+        //                     // response.data = JSON.parse(response.data);
+
+        //                 }
+        //             });
+        //         }, 500);
+        //     }
+        // };
+
+        // model.returnFormatArray = function(arr) {
+        //     var option = [];
+        //     if (arr.length > 0) {
+        //         option.push({ "label": "--select--", "title": "--select--", "value": "" });
+        //         _.each(arr, function(item) {
+        //             option.push({ "label": item.NAME, "title": item.NAME, "value": item.ID });
+        //         });
+        //     }
+        //     return option;
+        // };
         return model.init();
     }
 
@@ -1867,49 +1867,49 @@ app.directive('setClassWhenAtTop', function($window) {
         model.newProfessionCatgory = [];
 
         model.init = function() {
-            // model.ProfCatgoryf();
-            // model.ProfessionGroupf();
-            // model.newProfessionCatgoryf();
+            model.ProfCatgoryf();
+            model.ProfessionGroupf();
+            model.newProfessionCatgoryf();
             return model;
         };
 
-        // model.ProfCatgoryf = function() {
-        //     if (model.ProfCatgory.length === 0) {
-        //         serviceApp.ProfessionCatgory().then(function(response) {
-        //             var option = [];
-        //             option.push({ "label": "--select--", "title": "--select--", "value": "" });
-        //             _.each(response.data, function(item) {
-        //                 option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
-        //             });
-        //             model.ProfCatgory = option;
-        //         });
-        //     }
-        // };
-        // model.ProfessionGroupf = function() {
-        //     if (model.ProfGroup.length === 0) {
-        //         serviceApp.ProfessionGroup().then(function(response) {
-        //             var option = [];
-        //             option.push({ "label": "--select--", "title": "--select--", "value": "" });
-        //             _.each(response.data, function(item) {
-        //                 option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
-        //             });
-        //             model.ProfGroup = option;
-        //         });
-        //     }
-        // };
+        model.ProfCatgoryf = function() {
+            if (model.ProfCatgory.length === 0) {
+                serviceApp.ProfessionCatgory().then(function(response) {
+                    var option = [];
+                    option.push({ "label": "--select--", "title": "--select--", "value": "" });
+                    _.each(response.data, function(item) {
+                        option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+                    });
+                    model.ProfCatgory = option;
+                });
+            }
+        };
+        model.ProfessionGroupf = function() {
+            if (model.ProfGroup.length === 0) {
+                serviceApp.ProfessionGroup().then(function(response) {
+                    var option = [];
+                    option.push({ "label": "--select--", "title": "--select--", "value": "" });
+                    _.each(response.data, function(item) {
+                        option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+                    });
+                    model.ProfGroup = option;
+                });
+            }
+        };
 
-        // model.newProfessionCatgoryf = function() {
-        //     if (model.newProfessionCatgory.length === 0) {
-        //         serviceApp.newProfessionCat().then(function(response) {
-        //             var option = [];
-        //             option.push({ "label": "--select--", "title": "--select--", "value": 0 });
-        //             _.each(response.data, function(item) {
-        //                 option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
-        //             });
-        //             model.newProfessionCatgory = option;
-        //         });
-        //     }
-        // };
+        model.newProfessionCatgoryf = function() {
+            if (model.newProfessionCatgory.length === 0) {
+                serviceApp.newProfessionCat().then(function(response) {
+                    var option = [];
+                    option.push({ "label": "--select--", "title": "--select--", "value": 0 });
+                    _.each(response.data, function(item) {
+                        option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+                    });
+                    model.newProfessionCatgory = option;
+                });
+            }
+        };
 
         return model.init();
     }
@@ -1929,22 +1929,22 @@ app.directive('setClassWhenAtTop', function($window) {
         var option = [];
         model.caste = [];
         model.init = function() {
-            // model.casteselectf();
+            model.casteselectf();
             return model;
         };
 
-        // model.casteselectf = function() {
-        //     if (model.caste.length === 0) {
-        //         serviceApp.casteselect().then(function(response) {
-        //             option = [];
-        //             option.push({ "label": "--select--", "title": "--select--", "value": "" });
-        //             _.each(response.data, function(item) {
-        //                 option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
-        //             });
-        //             model.caste = option;
-        //         });
-        //     }
-        // };
+        model.casteselectf = function() {
+            if (model.caste.length === 0) {
+                serviceApp.casteselect().then(function(response) {
+                    option = [];
+                    option.push({ "label": "--select--", "title": "--select--", "value": "" });
+                    _.each(response.data, function(item) {
+                        option.push({ "label": item.Name, "title": item.Name, "value": item.ID });
+                    });
+                    model.caste = option;
+                });
+            }
+        };
 
         // model.empNamesSelectf = function() {
         //     serviceApp.EmpBinding(1, 2, '').then(function(response) {
