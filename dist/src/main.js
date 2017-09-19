0fc49cf0dc1f327eeb4d619535fde53c43edf634
umakusumuru <kusumuruuma@gmail.com>
2017-09-19 12:27:16 +0530

    dist added

--- dist/src/main.js
+++ dist/src/main.js
@@ -8630,7 +8630,7 @@ app.controller("commonviewfullprofile", ['customerDashboardServices', '$scope', 
                     var oppositegender = scope.Fromgender === 1 ? 'Ms.' : 'Mr.';
                     var oppositeshe = scope.Fromgender === 2 ? 'He' : 'She';
                     var oppositeher = scope.Fromgender === 2 ? 'his' : 'her';
-                    scope.titleproceed = "Proceed-" + oppositegender + " " + scope.personalinfo[0].NAME + " will be receiving your positive reply on proceeding further with " + oppositeher + " profile and your relationship manager will be working on this simultaneously to take it ahead";
+                    scope.titleproceed = oppositegender + " " + scope.personalinfo[0].NAME + " will be receiving your positive reply on proceeding further with " + oppositeher + " profile and your relationship manager will be working on this simultaneously to take it ahead";
                     scope.titledontproceed = "This sends a message to your relationship manager not to proceed further with " + oppositegender + " " + scope.personalinfo[0].NAME + " profile";
                     var photocount = scope.personalinfo[0].PhotoName_Cust;
                     scope.horoscopeimage = scope.personalinfo[0].HoroscopeImage === "" ||
@@ -8936,8 +8936,10 @@ app.controller("commonviewfullprofile", ['customerDashboardServices', '$scope', 
                     customerviewfullprofileservices.UpdateExpressIntrestViewfullprofile(MobjViewprofile).then(function(response) {
                         switch (response.data) {
                             case 1:
-                                if (scope.unpaidflag || scope.unpaidflagrenewal) {
-                                    scope.modalbodyID1 = scope.unpaidflag === true ?
+                               // if (scope.unpaidflag || scope.unpaidflagrenewal) {
+                                     //scope.modalbodyID1 = scope.unpaidflag === true ?
+                                    if (scope.lblpaid === "UnPaid" ||scope.lblpaid === "Renual") {
+                                         scope.modalbodyID1 = scope.lblpaid === "UnPaid" ?
                                         "Be our paid member to view this complete profile and for our assistance in proceeding with this match" +
                                         "For further assistance feel free to contact your relationship manager Mr.Kumar :91-9392696969" : "Your memebership points got exhausted so please do the payment to upgrade points." +
                                         "For further assistance feel free to contact your relationship manager Mr.Kumar :91-9392696969";
