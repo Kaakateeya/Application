130efe59bda6694ae9d3e28707b5072716799287
Your Name <kusumuruuma@gmail.com>
2017-12-04 10:46:42 +0530

    profile settings for deleted customer

--- app/modules/static/profileSettings.js
+++ app/modules/static/profileSettings.js
@@ -158,8 +158,8 @@ app.controller("profilesettings", ['$scope', '$mdDialog', 'customerProfilesettin
 
             customerProfilesettings.deleteprofile(ProfileID, Narrtion).then(function(response) {
                 if (response.data == 1) {
-                    authSvc.logout();
                     alerts.open('Delete Profile successfully', 'success');
+                    authSvc.logout();
                     scope.Resetallfields('deleteprofiles');
                 } else {
                     alerts.open('Delete Profile failed', 'warning');
