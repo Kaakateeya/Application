62aecdd970f5d4855da94f15f65ff1beaf7da28c
lakshmidevi407 <lakshmidevi407@gmail.com>
2018-01-04 13:33:11 +0530

    add... tocken

--- app/modules/homePage/homePage.js
+++ app/modules/homePage/homePage.js
@@ -19,7 +19,7 @@ app.controller('home', ['$scope', 'homepageservices', 'authSvc', 'successstories
             }, 500);
             scope.successstoriesarray = [];
             //http://183.82.0.58:3000/getTempToken
-            $http.post('http://localhost:3000/getTempToken', JSON.stringify({ source: 'Kaakateeya' }))
+            $http.post('http://183.82.0.58:3000/getTempToken', JSON.stringify({ source: 'Kaakateeya' }))
                 .then(function(response) {
                     if (response.data) {
                         sessionStorage.setItem('token', response.data.token);
